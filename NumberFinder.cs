ca259970b2a64d70bc1359b0935c86b0f3c8016c
Max <dranik15@mail.ru>
2019-02-25 18:07:48 +0300

    Update NumberFinder.cs

--- NumberFinder.cs
+++ NumberFinder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PadawansTask6
 {
@@ -6,8 +7,53 @@ namespace PadawansTask6
     {
         public static int? NextBiggerThan(int number)
         {
-            // put your code here
-            throw new NotImplementedException();
+            List<int> digits = new List<int>();
+            for (; number != 0; number /= 10)
+            {
+                digits.Add(number % 10);
+            }
+            int[] arr = digits.ToArray();
+            Array.Reverse(arr);
+
+            int i;
+            for (i = arr.Length - 1; i > 0; i--)
+            {
+                if (arr[i] > arr[i - 1])
+                {
+                    break;
+                }
+            }
+
+            if (i == 0)
+            {
+
+            }
+            else
+            {
+                int x = arr[i - 1], min = i;
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] > x && arr[j] < arr[min])
+                    {
+                        min = j;
+                    }
+                }
+
+                Swap(ref arr[i - 1], ref arr[min]);
+                Array.Sort(arr, i, arr.Length-i);
+            }
+            return Int32.Parse(string.Join("", arr));
         }
+
+        static void Swap<T>(ref T a, ref T b)
+        {
+            T temp;
+            temp = a;
+            a = b;
+            b = temp;
+        }
+
     }
 }
+
